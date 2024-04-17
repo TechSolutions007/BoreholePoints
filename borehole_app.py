4911d01fa74fcf8dd808d6ab26fa51621ef431cf
TechSolutions007 <moey.tee@gmail.com>
2024-04-17 21:46:47 +0200

    Update borehole_app.py

--- borehole_app.py
+++ borehole_app.py
@@ -1,31 +1,38 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+import numpy as np
+
+
 
 st.title('911 Kwa Dukuza | Borehole Points Stanger')
 
+
 url = "https://docs.google.com/spreadsheets/d/1lHI0ofOrxTorUrthTqJKafWbZgmPqf9QdeWyVotcbrQ/edit?usp=sharing"
 
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Disable caching for data retrieval
-@st.cache(allow_output_mutation=True)
-def fetch_data(url):
-    conn = st.connection("gsheets", type=GSheetsConnection)
-    return conn.read(spreadsheet=url)
+data = conn.read(spreadsheet=url)
 
-data = fetch_data(url)
 
-# Filtering options
-st.sidebar.header('Filter Points')
-selected_column = st.sidebar.selectbox('Select Column', ['All'] + data.columns.tolist())
-selected_value = st.sidebar.text_input('Search Term')
+# Display filtering options using columns
+with st.expander("Filter Points", expanded=True):
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        selected_column = st.selectbox('Select Column', ['All'] + data.columns.tolist())
+    with col2:
+        selected_value = st.text_input('Search Term')
 
 # Apply filters
 filtered_data = data.copy()
 if selected_column != 'All':
      filtered_data = filtered_data[filtered_data[selected_column].str.contains(selected_value, case=False).fillna(False).astype(bool)]
 
-# Display filtered DataFrame
-st.table(filtered_data)
+#Display filtered DataFrame
+st.dataframe(filtered_data,use_container_width=True,hide_index=True)
 
-st.text('The community are urged to please be mindful and courteous to those that are making water available to you')
-st.text('To add your name to the list to make water available to the community please contact Salim Tootla 083 662 8118')
+
+
+
+st.text('The community are urged to please be mindful and courteous to those that are making\nwater available to you')
+st.text('To add your name to the list to make water available to the community please contact\nSalim Tootla @ 083 662 8118')
