27e08d2da88bf3375fe65906956befd23e3ebdae
TechSolutions007 <moey.tee@gmail.com>
2024-04-17 22:02:15 +0200

    Update borehole_app.py

--- borehole_app.py
+++ borehole_app.py
@@ -13,7 +13,7 @@ url = "https://docs.google.com/spreadsheets/d/1lHI0ofOrxTorUrthTqJKafWbZgmPqf9Qd
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 # Disable caching for data retrieval
-@st.cache(allow_output_mutation=True)
+st.cache_data(ttl=1)
 def fetch_data(url):
     conn = st.connection("gsheets", type=GSheetsConnection)
     return conn.read(spreadsheet=url)
