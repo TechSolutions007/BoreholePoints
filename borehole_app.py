fc12eaa6bae3a2dfe7a2ed6feaea0a8a0a41cdcf
TechSolutions007 <moey.tee@gmail.com>
2024-04-17 21:25:20 +0200

    Update borehole_app.py

--- borehole_app.py
+++ borehole_app.py
@@ -25,7 +25,7 @@ if selected_column != 'All':
      filtered_data = filtered_data[filtered_data[selected_column].str.contains(selected_value, case=False).fillna(False).astype(bool)]
 
 # Display filtered DataFrame
-st.dataframe(filtered_data)
+st.table(filtered_data)
 
 st.text('The community are urged to please be mindful and courteous to those that are making water available to you')
 st.text('To add your name to the list to make water available to the community please contact Salim Tootla 083 662 8118')
